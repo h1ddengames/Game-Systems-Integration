b5a0ccd90ed49d17f6e4dcb5141a4e41716d8472
h1ddengames <skarim3000@gmail.com>
2020-03-16 21:03:38 -0700

    Added asset, renamed/removed assets, updated AutomatedTask
    Added Simple UI & Icons by Madder.
    Removed Bandits - Pixel Art.
    Renamed all asset folders that were downloaded from the asset store to include the name of the creator.
    AutomatedTask is no longer a monobehaviour but instead a normal C# class with Serializable.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Automation/AutomatedTask.cs
@@ -0,0 +1,63 @@
+﻿// Created by h1ddengames
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using SFB;
+using NaughtyAttributes;
+using RotaryHeart.Lib.SerializableDictionary;
+
+namespace h1ddengames {
+    [Serializable]
+    public class AutomatedTask {
+        #region Exposed Fields
+        [SerializeField] private int id;
+        [SerializeField] private UnityEvent task;
+        [SerializeField] private float delayBeforeTask;
+        [SerializeField] private float delayAfterTask;
+        [SerializeField] private bool hasInvokedTask = false;
+        [SerializeField] private bool hasFinishedDelayAfterTask = false;
+        #endregion
+
+        #region Private Fields
+        private float delayBeforeTaskBackup;
+        private float delayAfterTaskBackup;
+        #endregion
+
+        #region Getters/Setters/Constructors
+        public AutomatedTask(int id, UnityEvent task, float delayBefore, float delayAfter) {
+            Id = id;
+            Task = task;
+            DelayBeforeTask = delayBefore;
+            DelayAfterTask = delayAfter;
+            HasInvokedTask = false;
+            HasFinishedDelayAfterTask = false;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public UnityEvent Task { get => task; set => task = value; }
+        public float DelayBeforeTask { get => delayBeforeTask; set => delayBeforeTask = value; }
+        public float DelayAfterTask { get => delayAfterTask; set => delayAfterTask = value; }
+        public bool HasInvokedTask { get => hasInvokedTask; set => hasInvokedTask = value; }
+        public bool HasFinishedDelayAfterTask { get => hasFinishedDelayAfterTask; set => hasFinishedDelayAfterTask = value; }
+        #endregion
+
+        #region My Methods
+        public void Reset() {
+            DelayBeforeTask = delayBeforeTaskBackup;
+            DelayAfterTask = delayAfterTaskBackup;
+            HasInvokedTask = false;
+            HasFinishedDelayAfterTask = false;
+        }
+        #endregion
+
+        #region Unity Methods
+        void OnEnable() {
+            delayBeforeTaskBackup = DelayBeforeTask;
+            delayAfterTaskBackup = DelayAfterTask;
+        }
+        #endregion
+    }
+}
