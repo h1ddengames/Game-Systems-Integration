784dec71f48e77b779a21454a984a4936c29a713
h1ddengames <skarim3000@gmail.com>
2020-03-17 19:58:22 -0700

    Added animations, attack controller
    Added Sword Attack animation (default vertical sword slash)
    Added Shield Bash animation (default horizontal shield forward push)
    Added AttackController that will run an attack animation based on player input.
    
    Signed-off-by: h1ddengames <skarim3000@gmail.com>

---
+++ Assets/Scripts/Skills/AttackController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+using SFB;
+using DG.Tweening;
+using NaughtyAttributes;
+using RotaryHeart.Lib.SerializableDictionary;
+
+namespace h1ddengames {
+	public class AttackController : MonoBehaviour {
+		#region Exposed Fields
+		Animator playerAnimator;
+		#endregion
+		
+		#region Private Fields
+		#endregion
+		
+		#region Getters/Setters/Constructors
+		#endregion
+		
+		#region My Methods
+		#endregion
+		
+		#region Unity Methods
+		void OnEnable() {
+			
+		}
+		
+		void Start() {
+			playerAnimator = GetComponent<Animator>();
+		}
+
+		void Update() {
+			if(Input.GetKeyDown(KeyCode.T) || Input.GetKey(KeyCode.T)) {
+				playerAnimator.Play("Player Sword Attack Animation");
+			}
+
+			if (Input.GetKeyDown(KeyCode.G) || Input.GetKey(KeyCode.G)) {
+				playerAnimator.Play("Player Shield Bash Animation");
+			}
+		}
+		
+		void OnDisable() {
+			
+		}
+		#endregion
+		
+		#region Helper Methods
+		#endregion
+	}
+}
